95df5d0e5157f4bfa9ffc261d2879d4730ae74ff
James <james.simpson2@education.wa.edu.au>
2018-07-23 08:09:14 +0800

    Created initial botfile

---
+++ bot.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='^', description='Not a Hookerbot.')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.cmmands()
+async def cat(ctx):
+    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
